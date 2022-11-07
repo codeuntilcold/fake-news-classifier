5450c36191c6843cc4b7f5b3c2e9f53f6b0a8646
codeuntilcold <quocdungngole@gmail.com>
2022-11-07 23:25:39 +0700

    render word importance

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pickle
 from nltk.corpus import stopwords
 import re
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 
 app = Flask(__name__)
 ps = PorterStemmer()
@@ -11,38 +12,55 @@ ps = PorterStemmer()
 model = pickle.load(open('Model/logreg_fakenews.pickle', 'rb'))
 tfidfvect = pickle.load(open('Model/tfidf.pickle', 'rb'))
 
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
+
 def predict(text, title, author):
-    content = title + ' ' + author + ' ' + text 
+    content = title + ' ' + author + ' ' + text
     print(content)
     review = re.sub('[^a-zA-Z]', ' ', content)
     review = review.lower()
     review = review.split()
-    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = [ps.stem(word)
+              for word in review if not word in stopwords.words('english')]
     review = ' '.join(review)
 
     review_vect = tfidfvect.transform([review]).toarray()
-    prediction = 'giả' if model.predict(review_vect)[0] == 0 else 'thật'
+    prediction = 'giả' if model.predict(review_vect)[0] else 'thật'
     print(model.predict(review_vect))
-    return prediction
+
+    diction = measure_importance(review_vect)
+    # print(diction)
+    return prediction, diction
+
 
 @app.route('/', methods=['POST'])
 def webapp():
     text = request.form['textInput']
     author = request.form['authorInput']
     title = request.form['titleInput']
-    prediction = predict(text, title, author)
-    return render_template('index.html', textInput=text, result=prediction)
+    prediction, diction = predict(text, title, author)
+    sorted_dict = {k: v for k, v in sorted(diction.items(), key=lambda item: item[1])}
+    return render_template('index.html', original=text, textInput=text, result=prediction, dis=sorted_dict)
 
 
-@app.route('/predict/', methods=['GET','POST'])
+@app.route('/predict/', methods=['GET', 'POST'])
 def api():
     text = request.args.get("text")
     prediction = predict(text)
     return jsonify(prediction=prediction)
 
+
+def measure_importance(review_vect):
+    word_indexes = np.argwhere(review_vect[0])
+    # print(word_indexes[:, 0])
+    word_importance = model.coef_[0][word_indexes[:, 0]]
+    # print(word_importance)
+    return dict(zip(tfidfvect.get_feature_names_out()[word_indexes][:, 0], np.tanh(word_importance)))
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
