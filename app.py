1a51c3012ab40eb8c94d73176de150b14b3b0c4a
phiiiihungg <90836142+hung2001qbvn@users.noreply.github.com>
2022-11-08 13:18:15 +0700

    init commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pickle
 from nltk.corpus import stopwords
 import re
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 
 app = Flask(__name__)
 ps = PorterStemmer()
@@ -17,26 +18,36 @@ def home():
     return render_template('index.html')
 
 def predict(text, title, author):
-    content = title + ' ' + author + ' ' + text 
-    print(content)
-    review = re.sub('[^a-zA-Z]', ' ', content)
-    review = review.lower()
+    content = title + ' ' + author + ' ' + text
+    tokens = re.sub('[^a-zA-Z]', ' ', content)
+    review = tokens.lower()
     review = review.split()
-    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = [ps.stem(word)
+              for word in review if not word in stopwords.words('english')]
     review = ' '.join(review)
 
     review_vect = tfidfvect.transform([review]).toarray()
-    prediction = 'thật' if model.predict(review_vect)[0] == 0 else 'giả'
+    prediction = 'giả' if model.predict(review_vect)[0] else 'thật'
     print(model.predict(review_vect))
-    return prediction
+
+    diction = measure_importance(review_vect)
+    # print(diction)
+    return prediction, diction, tokens
 
 @app.route('/', methods=['POST'])
 def webapp():
     text = request.form['textInput']
     author = request.form['authorInput']
     title = request.form['titleInput']
-    prediction = predict(text, title, author)
-    return render_template('index.html', textInput=text, result=prediction)
+    prediction, weighted_stems, tokens = predict(text, title, author)
+
+    coloring = dict()
+    for word in tokens.split():
+        stemmed = ps.stem(word.lower()) if not word in stopwords.words('english') else word.lower()
+        if stemmed in weighted_stems:
+            coloring[word] = weighted_stems[stemmed] if stemmed in weighted_stems else 0
+
+    return render_template('index.html', original=text, textInput=text, result=prediction, dict=coloring)
 
 
 @app.route('/predict/', methods=['GET','POST'])
@@ -45,5 +56,15 @@ def api():
     prediction = predict(text)
     return jsonify(prediction=prediction)
 
+def measure_importance(review_vect):
+    word_indexes = np.argwhere(review_vect[0])
+    word_values = review_vect[0, word_indexes][:, 0]
+    # print(word_values)
+    # print(word_indexes[:, 0])
+    word_coeffs = model.coef_[0][word_indexes[:, 0]]
+    word_importance = word_coeffs * word_values
+    # print(word_importance)
+    return dict(zip(tfidfvect.get_feature_names_out()[word_indexes][:, 0], word_importance))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
